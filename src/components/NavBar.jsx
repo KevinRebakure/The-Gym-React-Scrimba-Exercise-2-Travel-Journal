b9b7fa70ff45c1c929742743da6ce6dc451621af
Rebakure <kevinrebakure@gmail.com>
2024-07-18 11:26:09 +0200

    done ✅feat/navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+    return (
+      <header className="text-white">
+        <nav className="flex justify-center gap-x-[7px] bg-[#F55A5A] py-[20px]">
+          <img src="./Fill 213.svg" alt="" />
+          <p className="text-[14.46px] font-medium">my traveljournal.</p>
+        </nav>
+      </header>
+    );
+}
