9015bfdd6f653665c5aeb3fd15dd6c70b99c1a26
Rebakure <kevinrebakure@gmail.com>
2024-07-18 11:30:19 +0200

    added images sources

--- src/notes.jsx
+++ src/notes.jsx
@@ -1,6 +1,6 @@
 export default [
   {
-    image: "",
+    image: "https://source.unsplash.com/WLxQvbMyfas",
     country: "JAPAN",
     place: "Mount Fuji",
     date: "12 Jan, 2021 - 24 Jan, 2021",
@@ -8,7 +8,7 @@ export default [
       "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
   },
   {
-    image: "",
+    image: "https://source.unsplash.com/JmuyB_LibRo",
     country: "AUSTRALIA",
     place: "Sydney Opera House",
     date: "27 May, 2021 - 8 Jun, 2021",
@@ -16,7 +16,7 @@ export default [
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
   },
   {
-    image: "",
+    image: "https://source.unsplash.com/3PeSjpLVtLg",
     country: "NORWAY",
     place: "Geirangerfjord",
     date: "01 Oct, 2021 - 18 Nov, 2021",
